45d3bae2fdb4ddce358d8c567b1a5c6d3719c948
Ting Chen <tingshen.chen@gmail.com>
2020-06-07 19:04:07 -0400

    Add linting

--- src/utils/code-validation.ts
+++ src/utils/code-validation.ts
@@ -1,12 +1,12 @@
-import { TransmitCodes } from '../constants/TransmitCodes'
+import { TransmitCodes } from '../constants/TransmitCodes';
 
-const validDeviceNumbers = Object.keys(TransmitCodes).map(n => parseInt(n))
-const validSignals = ['on', 'off']
+const validDeviceNumbers = Object.keys(TransmitCodes).map((n) => parseInt(n));
+const validSignals = ['on', 'off'];
 
 export function isValidDeviceNumber(deviceNumber: number) {
-  return validDeviceNumbers.includes(deviceNumber);
+    return validDeviceNumbers.includes(deviceNumber);
 }
 
 export function isValidSignal(signal: string) {
-  return validSignals.includes(signal);
+    return validSignals.includes(signal);
 }
--- src/utils/transmit-codes.ts
+++ src/utils/transmit-codes.ts
@@ -1,16 +1,20 @@
-import { exec } from 'child_process'
-import { TransmitCodes } from '../constants/TransmitCodes'
+import { exec } from 'child_process';
+import { TransmitCodes } from '../constants/TransmitCodes';
 
-const PULSE_LENGTH = 176
-const TRANSMIT_GPIO = 4
+const PULSE_LENGTH = 176;
+const TRANSMIT_GPIO = 4;
 
 export function transmitSignalToDevice(deviceNumber: number, signal: string) {
-  exec(`python3 send.py -g ${TRANSMIT_GPIO} -p ${PULSE_LENGTH} ${TransmitCodes[deviceNumber][signal]}`, function(error, _, stderror) {
-    if (error) {
-      console.log(error.stack);
-      console.log('Error code: ' + error.code);
-      console.log('Signal received: ' + error.signal);
-    }
-    console.log(stderror);
-  })
+    exec(`python3 send.py -g ${TRANSMIT_GPIO} -p ${PULSE_LENGTH} ${TransmitCodes[deviceNumber][signal]}`, function (
+        error,
+        _,
+        stderror,
+    ) {
+        if (error) {
+            console.log(error.stack);
+            console.log('Error code: ' + error.code);
+            console.log('Signal received: ' + error.signal);
+        }
+        console.log(stderror);
+    });
 }
