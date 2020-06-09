505d4ca72203b43fbb50ccce591fe5609b17863d
Ting Chen <tingshen.chen@gmail.com>
2020-06-09 01:14:59 -0400

    Add ts-node to fix dev. Add timer capability.

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,41 @@ import express from 'express';
 import { isValidDeviceNumber, isValidSignal } from './utils/code-validation';
 import { transmitSignalToDevice } from './utils/transmit-codes';
 
+interface DeviceTimers {
+  [key: number]: NodeJS.Timeout;
+}
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+const deviceTimers: DeviceTimers = {};
+
+function cancelExistingTimersForDevice(deviceNumber: number) {
+  if (!deviceTimers[deviceNumber]) return;
+  console.log(`Cancelled timer for device number ${deviceNumber}`);
+  clearTimeout(deviceTimers[deviceNumber]);
+  delete deviceTimers[deviceNumber];
+}
+
+function setNewTimerForDevice(deviceNumber: number, timeout: number) {
+  cancelExistingTimersForDevice(deviceNumber);
+  console.log(`Setting a new timer for device ${deviceNumber}`);
+  deviceTimers[deviceNumber] = setTimeout(() => {
+    transmitSignalToDevice(deviceNumber, 'off');
+  }, timeout);
+}
+
+app.use((_, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
 
 app.get('/transmit/', (req, res) => {
   const { query } = req;
-  const { deviceNumber, signal } = query;
+  const { deviceNumber, signal, timeout } = query;
 
   if (!isValidDeviceNumber(deviceNumber)) {
     res.status(400).send({
@@ -19,9 +48,28 @@ app.get('/transmit/', (req, res) => {
     });
   } else {
     transmitSignalToDevice(deviceNumber, signal);
-    res.send({
-      message: `Device ${deviceNumber} turned ${signal}`,
-    });
+    if (signal === 'off') {
+      cancelExistingTimersForDevice(deviceNumber);
+    }
+
+    // Check if there's a timeout param, if so, set a new timer to shutdown the device
+    if (timeout && typeof timeout === 'string') {
+      const timeToShutdown = parseInt(timeout);
+
+      if (!isNaN(timeToShutdown) && timeToShutdown > 0) {
+        const toHours = timeToShutdown / 3600000;
+        setNewTimerForDevice(deviceNumber, timeToShutdown);
+        res.send({
+          message: `Device ${deviceNumber} turned ${signal} and will shut off in ${toHours.toFixed(
+            1,
+          )} hours`,
+        });
+      }
+    } else {
+      res.send({
+        message: `Device ${deviceNumber} turned ${signal}`,
+      });
+    }
   }
 });
 
